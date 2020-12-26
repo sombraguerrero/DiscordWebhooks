d68df6b0a58ad84c31bdb053bc8c5a0ce868dfef
Robert Setter <r.setter@comcast.net>
2020-12-26 08:35:26 -0600

    Minor fixes

--- lis2.js
+++ lis2.js
@@ -10,13 +10,13 @@ if (decision % 2 == 1) {
 	basePath += "/le-louvre-de-lis2/"
 }
 var imgList = fs.readdirSync(basePath);
-var selectedImg = imgList[Math.floor(Math.random() * imgList.length - 1)];
+var selectedImg = imgList[Math.floor(Math.random() * imgList.length)];
 
 //Perform post to Discord
 var formData = new FormData();
 formData.append('content', 'Random fan art: ' + selectedImg);
 formData.append('file', fs.createReadStream(basePath + selectedImg), { filename: selectedImg});
-formData.submit('https://discordapp.com/api/webhooks/768447672758566919/{{pl_wolfbros}}', (err, res) => {
+formData.submit('https://discordapp.com/api/webhooks/768447672758566919/{{wolfbros_token}}', (err, res) => {
 		var myLog = fs.createWriteStream('log.txt');
 		myLog.write("Response code: " + res.statusCode + "\r\n" + err);
 });
