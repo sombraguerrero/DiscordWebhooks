135c8cfd294bff80066a5b1ce18988db9b7f1afd
Robert Setter <r.setter@comcast.net>
2020-12-27 19:31:03 -0600

    consolidating to use one random value for fanart

--- lis2.js
+++ lis2.js
@@ -4,19 +4,19 @@ const fs = require('fs');
 const FormData = require('form-data');
 
 // Navigate to and retrieve random file.
-var decision = Math.floor(Math.random() * 10);
+var num = Math.random();
 var basePath = "/var/services/homes/bobertdos/Google_Drive/WD_lis-media/";
-if (decision % 2 == 1) {
+if (Math.floor(num * 10) % 2 == 1) {
 	basePath += "/le-louvre-de-lis2/"
 }
 var imgList = fs.readdirSync(basePath);
-var selectedImg = imgList[Math.floor(Math.random() * imgList.length)];
+var selectedImg = imgList[Math.floor(num * imgList.length)];
 
 //Perform post to Discord
 var formData = new FormData();
 formData.append('content', 'Random fan art: ' + selectedImg);
 formData.append('file', fs.createReadStream(basePath + selectedImg), { filename: selectedImg});
-formData.submit('https://discordapp.com/api/webhooks/768447672758566919/{{wolfbros_token}}', (err, res) => {
+formData.submit('https://discordapp.com/api/webhooks/768447672758566919/{{pl_wofbros}}', (err, res) => {
 		var myLog = fs.createWriteStream('log.txt');
 		myLog.write("Response code: " + res.statusCode + "\r\n" + err);
 });
