b1c4639dd08c378fe336402416d9a5874c61ee26
Robert Setter <r.setter@comcast.net>
2021-01-17 02:18:20 -0600

    CSV reading and reorganization
    Added pipe-delimited file reading and reorganized the code structure to simplify future enhancements.

--- keystone.js
+++ keystone.js
@@ -1,85 +1,138 @@
 'use strict';
 const https = require('https');
+const fs = require('fs');
+function getKeyResponse() {
+	fs.readFile('/var/services/web/webhooks/responses.csv', 'utf8', function(err, data) {
+		var output = data.split('|');
+		//console.log(output);
+	
+	var keyStone = new Object();
+	keyStone.content = "<@user> " + output[Math.floor(Math.random() * output.length)];
+	var postString = JSON.stringify(keyStone);
+	try {
+		  const discordOptions = {
+			hostname: 'discord.com',
+			path: '/api/webhooks/747963105241202800/{{pl_botspam}}',
+			method: 'POST',
+			headers: {
+			  'Content-Type': 'application/json',
+			  'Content-Length': Buffer.byteLength(postString)
+			}
+		  };
 
-// Stage Get request to retrieve data from either dad jokes or facts API
-var decision = Math.floor(Math.random() * 10);
-var target = "icanhazdadjoke.com";
-var targetpath = "";
-var rockFact = false;
-if (decision % 2 == 1) {
-	target = "uselessfacts.jsph.pl";
-	targetpath = '/random.txt?language=en'
-	rockFact = true;
-}
-const contentOptions = {
-        hostname: target,
-        path: targetpath,
-        method: 'GET',
-        headers: {
-          'Accept': 'text/plain',
-		  'User-Agent': 'Discord_Webhook/1.2 (https://github.com/sombraguerrero/RandomFanArt ;robert.setter@bobertdos.me)'
-        }
-      };
+		  const discordReq = https.request(discordOptions, (res) => {
+			console.log(`STATUS: ${res.statusCode}`);
+			console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+			res.setEncoding('utf8');
 
-//Perform GET request with specified options. (Note that the aliased functions automatically call end() on the request object.)
-const contentReq = https.request(contentOptions, (res) => {
-  const { statusCode } = res;
-  const contentType = res.headers['content-type'];
+			res.on('data', (chunk) => {
+			  console.log(`BODY: ${chunk}`);
+			});
+			res.on('end', () => {
+			  console.log('No more data in response.');
+			});
+		  });
 
-  // Stage POST request to Discord Webhook
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => {
-    try {
-		var postData = new Object();
-		postData.content = "<@540850957131579413> " + rawData;
-		if (rockFact) {
-			var rockFactLines = rawData.split(/\r?\n/);
-			postData.content = "<@540850957131579413> " + rockFactLines[0].slice(2).replace(/`/g, '\'');
+		  discordReq.on('error', (e) => {
+			console.error(`problem with request: ${e.message}`);
+		  });
+
+		  // Write data to request body
+		  discordReq.write(postString);
+		  //Since the request method is being used here for the post, we're calling end() manually on both request objects.
+		  discordReq.end();
+		  console.log(postString);
 		}
-		var postString = JSON.stringify(postData);
-      const discordOptions = {
-        hostname: 'discord.com',
-        path: '/api/webhooks/747963105241202800/{{pl_botspam}}',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': Buffer.byteLength(postString)
-        }
-      };
+		catch (e) {
+		  console.error(e.message);
+		}
+	});
+}
 
-      const discordReq = https.request(discordOptions, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
+function pullStuff(rockFact, target, targetpath) {
+	const contentOptions = {
+			hostname: target,
+			path: targetpath,
+			method: 'GET',
+			headers: {
+			  'Accept': 'text/plain',
+			  'User-Agent': 'Discord_Webhook/1.2 (https://github.com/sombraguerrero/RandomFanArt ;robert.setter@bobertdos.me)'
+			}
+		  };
+		  //console.log(contentOptions);
 
-        res.on('data', (chunk) => {
-          console.log(`BODY: ${chunk}`);
-        });
-        res.on('end', () => {
-          console.log('No more data in response.');
-        });
-      });
+	//Perform GET request with specified options. (Note that the aliased functions automatically call end() on the request object.)
+	const contentReq = https.request(contentOptions, (res) => {
+	  const { statusCode } = res;
+	  const contentType = res.headers['content-type'];
 
-      discordReq.on('error', (e) => {
-        console.error(`problem with request: ${e.message}`);
-      });
+	  // Stage POST request to Discord Webhook
+	  res.setEncoding('utf8');
+	  let rawData = '';
+	  res.on('data', (chunk) => { rawData += chunk; });
+	  res.on('end', () => {
+		try {
+			var postData = new Object();
+			postData.content = "<@user> " + rawData;
+			if (rockFact) {
+				var rockFactLines = rawData.split(/\r?\n/);
+				postData.content = "<@user> " + rockFactLines[0].slice(2).replace(/`/g, '\'');
+			}
+			var postString = JSON.stringify(postData);
+		  const discordOptions = {
+			hostname: 'discord.com',
+			path: '/api/webhooks/747963105241202800/{{pl_botspam}}',
+			method: 'POST',
+			headers: {
+			  'Content-Type': 'application/json',
+			  'Content-Length': Buffer.byteLength(postString)
+			}
+		  };
 
-      // Write data to request body
-      discordReq.write(postString);
-	  //Since the request method is being used here for the post, we're calling end() manually on both request objects.
-      discordReq.end();
-      console.log(postString);
-    } catch (e) {
-      console.error(e.message);
-    }
-  });
-});
+		  const discordReq = https.request(discordOptions, (res) => {
+			console.log(`STATUS: ${res.statusCode}`);
+			console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+			res.setEncoding('utf8');
 
-//Using request method for the get too, so calling end() here too.
-contentReq.end();
+			res.on('data', (chunk) => {
+			  console.log(`BODY: ${chunk}`);
+			});
+			res.on('end', () => {
+			  console.log('No more data in response.' + "\r\nThis is for " + target);
+			});
+		  });
 
-contentReq.on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
-});
+		  discordReq.on('error', (e) => {
+			console.error(`problem with request: ${e.message}`);
+		  });
+
+		  // Write data to request body
+		  discordReq.write(postString);
+		  //Since the request method is being used here for the post, we're calling end() manually on both request objects.
+		  discordReq.end();
+		  console.log(postString);
+		} catch (e) {
+		  console.error(e.message);
+		}
+	});
+	});
+	//Using request method for the get too, so calling end() here too.
+	contentReq.end();
+	contentReq.on('error', (e) => {
+	  console.error(`Got error: ${e.message}`);
+	});
+}
+
+var decision = Math.floor(Math.random() * 12);
+if (decision % 3 == 0) {
+	//console.log('JOKE!!!');
+	pullStuff(false, 'icanhazdadjoke.com', '');
+}
+else if (decision % 3 == 1) {
+	//console.log('ROCK FACT!!!');
+	pullStuff(true, 'uselessfacts.jsph.pl', '/random.txt?language=en');
+}
+else {
+	//console.log('FORM RESPONSE!!!');
+	getKeyResponse();
+}
