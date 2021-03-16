fc75390c01f95c1a2cd3b5dd659e2cfc16ddef3c
Robert Setter <r.setter@comcast.net>
2021-03-16 09:24:28 -0500

    Removed copyright from footer,
    Kept date with label.

--- keystone.js
+++ keystone.js
@@ -494,7 +494,7 @@ function NasaAPOD(apodDate) {
 			var myAuthor = new Object();
 			myAuthor.name = parsedData.copyright;
 			var myFooter = new Object();
-			myFooter.text = 'By: ' + parsedData.copyright + '\r\nPosted: ' + parsedData.date;
+			myFooter.text = 'Posted: ' + parsedData.date;
 			var myEmbed = new Object();
 			myEmbed.image = myImage;
 			myEmbed.author = myAuthor;
